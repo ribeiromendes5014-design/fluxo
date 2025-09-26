a4b4865bfa1bcce189a812596406128678d66eaa
ribeiromendes5014-design <ribeiromendes5014@gmail.com>
2025-09-25 23:50:45 -0300

    Update app.py

--- app.py
+++ app.py
@@ -164,7 +164,7 @@ else:
             sucesso = salvar_dados_no_github(df_atualizado, sha, COMMIT_MESSAGE_DELETE)
             if sucesso:
                 st.success(f"{len(indices_a_excluir)} movimentação(ões) excluída(s) com sucesso!")
-                st.experimental_rerun()
+                st.rerun()
             else:
                 st.error("Falha ao excluir movimentações.")
         else:
@@ -205,3 +205,4 @@ else:
             col1_f.metric("Entradas", f"R$ {entradas_filtro:,.2f}")
             col2_f.metric("Saídas", f"R$ {abs(saidas_filtro):,.2f}")
             col3_f.metric("Saldo", f"R$ {saldo_filtro:,.2f}")
+
